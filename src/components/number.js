f2bbc63f04586100ecb4e6a03dd2a873eb2a97a6
Benson Njuguna <njugunab655@gmail.com>
2023-04-26 12:57:30 +0300

    Handle events

--- src/components/number.js
+++ src/components/number.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Number = ({
-  value, styled,
+  value, styled, onclick,
 }) => (
-  <div className={styled}>
+  <button type="button" className={styled} onClick={() => { onclick(value); }}>
     {value}
-  </div>
+  </button>
 );
 
 Number.propTypes = {
-  value: PropTypes.number.isRequired,
+  value: PropTypes.string.isRequired,
   styled: PropTypes.string.isRequired,
+  onclick: PropTypes.func.isRequired,
 };
 
 export default Number;
