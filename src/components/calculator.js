d4e9ff1c96ac1794bffb668aeef2eea0dbab8ee6
Benson Njuguna <njugunab655@gmail.com>
2023-04-26 12:55:03 +0300

    Import and apply logic event handlers

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 import Btn from './btn';
 import Number from './number';
+// import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <section id="calculator">
-    <div id="output">
-      <input type="text" readOnly value={123} />
-    </div>
-    <div id="inputs-container">
-      <Btn value="AC" styled="btn normal" id="allClear" />
-      <Btn value="+/-" styled="btn normal" id="addSubtract" />
-      <Btn value="%" styled="btn normal" id="remainder" />
-      <Btn value="÷" styled="btn colored" id="divide" />
-      <Number value={7} styled="btn normal" />
-      <Number value={8} styled="btn normal" />
-      <Number value={9} styled="btn normal" />
-      <Btn value="x" styled="btn colored" id="multiply" />
-      <Number value={4} styled="btn normal" />
-      <Number value={5} styled="btn normal" />
-      <Number value={6} styled="btn normal" />
-      <Btn value="-" styled="btn colored" id="subtract" />
-      <Number value={1} styled="btn normal" />
-      <Number value={2} styled="btn normal" />
-      <Number value={3} styled="btn normal" />
-      <Btn value="+" styled="btn colored" id="add" />
-      <Number value={0} styled="btn normal dbl" />
-      <Btn value="." styled="btn normal" id="period" />
-      <Btn value="=" styled="btn colored" id="equals" />
-    </div>
-  </section>
-);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  function handleClick(value) {
+    console.log(value);
+    setState((prev) => {
+      const newObj = calculate(prev, value);
+      return newObj;
+    });
+  }
+
+  return (
+    <section id="calculator">
+      <div id="output">
+        <input type="text" readOnly value={state.total ?? 0} />
+        <input type="text" readOnly value={state.next ?? 0} />
+      </div>
+      <div id="inputs-container">
+        <Btn value="AC" styled="btn normal" id="allClear" onclick={(value) => handleClick(value)} />
+        <Btn value="+/-" styled="btn normal" id="addSubtract" onclick={(value) => handleClick(value)} />
+        <Btn value="%" styled="btn normal" id="remainder" onclick={(value) => handleClick(value)} />
+        <Btn value="÷" styled="btn colored" id="divide" onclick={(value) => handleClick(value)} />
+        <Number value="7" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Number value="8" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Number value="9" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Btn value="x" styled="btn colored" id="multiply" onclick={(value) => handleClick(value)} />
+        <Number value="4" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Number value="5" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Number value="6" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Btn value="-" styled="btn colored" id="subtract" onclick={(value) => handleClick(value)} />
+        <Number value="1" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Number value="2" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Number value="3" styled="btn normal" onclick={(value) => handleClick(value)} />
+        <Btn value="+" styled="btn colored" id="add" onclick={(value) => handleClick(value)} />
+        <Number value="0" styled="btn normal dbl" onclick={(value) => handleClick(value)} />
+        <Btn value="." styled="btn normal" id="period" onclick={(value) => handleClick(value)} />
+        <Btn value="=" styled="btn colored" id="equals" onclick={(value) => handleClick(value)} />
+      </div>
+    </section>
+  );
+};
 
 export default Calculator;
