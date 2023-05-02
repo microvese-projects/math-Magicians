00b7c7d7b74b95ac21b441f34049ba1fbafd39ae
Benson Njuguna <njugunab655@gmail.com>
2023-05-02 18:03:47 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/calculator';
 import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
-      <Quote />
+      <nav>
+        <ul>
+          <li>
+            <h1>Math Magicians</h1>
+          </li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/calculator">Calculator</NavLink>
+          </li>
+          <li>
+            <NavLink to="/quote">Quote</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
